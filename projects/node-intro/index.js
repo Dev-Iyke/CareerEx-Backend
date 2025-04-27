0ddc87ff6a34610e7cf7047ab8e7093634f97d62
Dev-Iyke <iyke.thompson01@gmail.com>
2025-04-27 19:47:49 +0100

    Started backend proper, creating apis and theier endpoints to send and receive data

---
+++ projects/node-intro/index.js
@@ -0,0 +1,52 @@
+const express = require("express")
+
+//Initialize an express app
+const app = express()
+
+//Beating the middleware - making request body accessible
+app.use(express.json())
+
+// Dynamically creating a port
+const port = process.env.PORT || 5000
+
+// Running the server, listening on specified port
+app.listen(port, (e) => {
+  if(e){
+    console.log('Error message: ', e)
+  } else {
+    console.log(`Server running on port ${port}`)
+  }
+})
+
+// APIs
+//HOMEPAGE
+app.get("/", (request, response) => {
+  response.send("Welcome to my first server with CareerEx!")
+})
+
+//GET USERS
+app.get("/users", (request, response) => {
+  const users = [
+    {
+      firstName: "James",
+      role: 'Admin'
+    },
+    {
+      firstName: "Thompson",
+      role: "learner"
+    }
+  ]
+  response.json(users)
+})
+
+//CREATE A USER
+app.post("/create-user", (request, response) => {
+  const newUser = request.body
+  console.log(newUser)
+    
+  response.json({
+    success: true,
+    message: "User received successfully!",
+    newUser
+  })
+})
